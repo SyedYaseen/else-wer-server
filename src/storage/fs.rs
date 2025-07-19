6f1697abdd82bb2a30cee5d1d24b5f77703a1d9e
syedyaseen <s.syedyaseen.s@gmail.com>
2025-07-19 12:26:55 +0100

    recursive scan complete

--- src/storage/fs.rs
+++ src/storage/fs.rs
@@ -1,24 +1,51 @@
 use std::collections::HashMap;
+use std::ffi::OsString;
 use std::fs;
 use std::env;
 use std::hash::Hash;
 use std::io;
+use std::path::PathBuf;
+use std::process;
 
-fn get_path() -> Result<String, env::VarError> {
+fn get_path() -> PathBuf {
     let key = "AUDIOBOOKS_LOCATION";
-    env::var(key)
+    
+    match env::var(key) {
+        Ok(path) if !path.trim().is_empty() => {
+            let p = PathBuf::from(path);
+            if !p.exists() {
+                if let Err(e) = fs::create_dir_all(&p) {
+                    eprintln!("Error: Failed to create directory {}. {}", p.display(), e);
+                    process::exit(1);
+                }
+            }
+            p
+        },
+        _ => {
+          eprintln!("Env {key} doesn't exist. Setting default audiobooks location");
+          let default_path = PathBuf::from("data");
+          
+          if !default_path.exists() {
+            if let Err(e) = fs::create_dir_all(&default_path) {
+                eprintln!("Error: Failed to create default directory {}. {}", default_path.display(), e);
+                process::exit(1);
+            }
+          }
+          default_path
+        } 
+    }
 }
 
 #[derive(Debug)]
 struct AudioBook {
     author: String,
-    series: String,
+    series: Option<String>,
     title: String,
-    content_path: String
+    content_path: OsString
 }
 
 impl AudioBook {
-    fn new(author: String, series: String, title: String, content_path: String ) -> AudioBook {
+    fn new(author: String, series: Option<String>, title: String, content_path: OsString ) -> AudioBook {
         AudioBook {
             author: author,
             series: series,
@@ -28,53 +55,65 @@ impl AudioBook {
     }
 }
 
+fn has_dirs(path: &PathBuf) -> Result<bool, io::Error> {
+    Ok(fs::read_dir(path)?
+        .filter_map(Result::ok)
+        .any(|dir| dir.file_type()
+            .map(|ft| ft.is_dir()).unwrap_or(false)))
+}
 
-
-fn recursive_dirscan(path: &String, audio_books: &mut Vec<AudioBook>) -> Result<(), io::Error> {
+fn recursive_dirscan(path: &PathBuf, audio_books: &mut Vec<AudioBook>) -> Result<(), io::Error> {
     let entries = fs::read_dir(path)?;
+
     for dir_entry in entries {
         let entry = dir_entry?;
         let f_name = entry.file_name();
         let f_type = entry.file_type()?;
+        
 
         if f_type.is_dir() {
-            let sub_dir = f_name.to_str().unwrap().to_string();
-            let sub_dir_path = format!("{}/{}", path, sub_dir);
+            let sub_dir = match f_name.to_str() {
+                Some(f_name) => f_name,
+                None => {
+                    println!("Unable to decode utf-8 variable f_name");
+                    continue;
+                }
+            }; 
+            
+            let mut sub_dir_path = PathBuf::from(path);
+            sub_dir_path.push(sub_dir);
+            
             if let Err(e) =  recursive_dirscan(&sub_dir_path, audio_books) {
                 println!("Err reading folder {e}");
             }
             
-            let mut split_paths = sub_dir_path.split("/");
-            split_paths.next();
+            let v: Vec<_> = sub_dir_path
+                .components()
+                .map(|c| c.as_os_str()
+                    .to_str()
+                    .unwrap_or("")
+                    .to_string()
+                    )
+                    .collect();
 
-            let author = match split_paths.next() {
-                Some(v) => v,
-                None => continue
-            };
+            // println!("{:#?}", v);
 
-            let mut series = match split_paths.next() {
-                Some(v) => v,
-                None => continue
-            };
-
-            let title = match split_paths.next() {
-                Some(v) => v,
-                None => {
-                    let title = series;
-                    series = "";
-                    title
-                }
-            };
-
-            if let Some(a) = audio_books.last() {
-                if a.series == title {
+            let (author, series, title): (String, Option<String>, String) = match v.as_slice() {
+                [_, author, series, title, ..] => (author.to_string(), Some(series.to_string()), title.to_string()),
+                [_, author, title, ..] => (author.to_string(), None, title.to_string()),
+                _ => { 
+                    println!("Warn: Not a valid path during directory scan");
                     continue;
                 }
+            };
+
+            let is_series = series == None && has_dirs(&sub_dir_path)?;
+            
+            if !is_series {
+                audio_books.push(
+                    AudioBook::new(author, series, title, sub_dir_path.into_os_string())
+                );
             }
- 
-            audio_books.push(
-                AudioBook::new(author.to_owned(), series.to_owned(), title.to_owned(), sub_dir_path)
-            );
         }
     }
 
@@ -82,12 +121,11 @@ fn recursive_dirscan(path: &String, audio_books: &mut Vec<AudioBook>) -> Result<
 }
 
 pub fn scan_for_audiobooks() -> Result<(), env::VarError> {
-    let path = get_path()?;
+    let path = get_path();
 
     let mut audio_books: Vec<AudioBook> = Vec::new();
-    // scan_base_dir(&path, &mut audio_books);
     let mut audio_books: Vec<AudioBook> = Vec::new();
-    recursive_dirscan(&path, &mut audio_books);
+    let _ = recursive_dirscan(&path, &mut audio_books);
 
     for i in &audio_books {
         println!("{:#?}", i);
